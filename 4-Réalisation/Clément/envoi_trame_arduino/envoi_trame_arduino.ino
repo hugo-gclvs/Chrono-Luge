a4961e6192899fa113db3a47f71695beef44cd54
JPMA2 <fontenayclement@laposte.net>
2021-02-08 12:11:10 +0100

    modif Clément

---
+++ 4-Réalisation/Clément/envoi_trame_arduino/envoi_trame_arduino.ino
@@ -0,0 +1,59 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.write("<");
+  Serial.write("I");
+  Serial.write("D");
+  Serial.write("0");
+  Serial.write("0");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("L");
+  Serial.write("1");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("P");
+  Serial.write("A");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("F");
+  Serial.write("E");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("M");
+  Serial.write("A");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("W");
+  Serial.write("C");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("F");
+  Serial.write("E");
+  Serial.write(">");
+
+  Serial.write("<");
+  Serial.write("b");
+  Serial.write("o");
+  Serial.write("n");
+  Serial.write("s");
+  Serial.write("o");
+  Serial.write("i");
+  Serial.write("r");
+
+  Serial.write("1");
+  Serial.write("5");
+
+  Serial.write("<");
+  Serial.write("E");
+  Serial.write(">");
+
+}
