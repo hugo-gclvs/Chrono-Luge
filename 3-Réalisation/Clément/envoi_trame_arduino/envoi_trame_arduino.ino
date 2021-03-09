9366d4cf9ac2a7632cf05bc8947ddab40f8132a9
JPMA2 <78344520+JPMA2@users.noreply.github.com>
2021-03-09 17:27:11 +0100

    oui

--- 3-Réalisation/Clément/envoi_trame_arduino/envoi_trame_arduino.ino
+++ 3-Réalisation/Clément/envoi_trame_arduino/envoi_trame_arduino.ino
@@ -1,38 +1,26 @@
 void setup() {
-  #define RX1 19
-  #define TX1 18
-  pinMode(RX1, INPUT);
-  pinMode(TX1, OUTPUT);
+#define RX1 19
+#define TX1 18
+  pinMode(19, INPUT);
+  pinMode(18, OUTPUT);
   //initialisation du port série
   Serial.begin(9600);
+
+//Serial.write("<ID00><BE><05><E>");
+Serial.println("bonjour");
+  //trame pour l'affichage du message "oui"
+  Serial.write("<ID00><L1><PA><FE><MA><WC><FE><CA>non1B<E>"); // envoi de la première partie de la trame (code non variant utile à la configuration de l'affichage
+  //Serial.write("non"); // envoi du texte à afficher
+  //Serial.write("1B"); //valeur du checksum
+  //
+  Serial.write("<E>");
+
 
 }
 
 void loop() {
 
-  //Serial.write("<ID00><BE><05><E>"); // trame de début
-
-  //trame pour l'affichage du message "Hello"
-  Serial1.write("<ID00><L1><PA><FE><MA><WC><FE>Hello36<E>"); // envoi de la première partie de la trame (code non variant utile à la configuration de l'affichage
- // Serial1.write("Hello"); // envoi du texte à afficher
-  //Serial1.write("36"); //valeur du checksum
-  //Serial1.write("<E>");
-
-  //trame pour l'affichage du message "oui"
- /* Serial.write("<ID00><L1><PA><FE><MA><WC><FE><CA"); // envoi de la première partie de la trame (code non variant utile à la configuration de l'affichage
-  Serial.write("oui"); // envoi du texte à afficher
-  Serial.write("07"); //valeur du checksum
-  Serial.write("<E>");*/
-
-//trame pour l'affichage du message "Test" en rouge 
-/*Serial1.write("<ID00><L1><PA><FE><MA><WC><FE><CA>"); // envoi de la première partie de la trame (code non variant utile à la configuration de l'affichage
-  Serial1.write("Test"); // envoi du texte à afficher
-  Serial1.write("42"); //valeur du checksum
-  Serial1.write("<E>");*/
   
-  delay(1000);
 
- 
- 
 
 }
