9c77c8a3f1c9bc6cfb2e01f1b97c424d2617f67d
JPMA2 <78344520+JPMA2@users.noreply.github.com>
2021-03-22 12:09:18 +0100

    ytr
    ghdtxw

--- 3-Réalisation/Clément/affichage ESP32 ecran/affichage_esp32_ecran/affichage_esp32_ecran.ino
+++ 3-Réalisation/Clément/affichage ESP32 ecran/affichage_esp32_ecran/affichage_esp32_ecran.ino
@@ -9,7 +9,7 @@ void setup () {
 
 void loop () {
 
-  Serial.print("<ID00><BE>05<E><ID00><L1><PA><FE><MA><WC><FE><CD>bonjour10<E><ID00><BF>06<E>");                      //<L1> Décris la ligne du message, <PA> la page, <FA> le sens du message ? <MA> les paramètres d'affichage, <WC> la durée du message  <FE>"message"<FE>
+  Serial.print("<ID00><BE>05<E><ID00><L1><PA><FE><MA><WC><FE><CA>bonjour15<E><ID00><BF>06<E>");                      //<L1> Décris la ligne du message, <PA> la page, <FA> le sens du message ? <MA> les paramètres d'affichage, <WC> la durée du message  <FE>"message"<FE>
   //Serial1.print("test");//message à affiché
   //Serial1.print("62");//checksum Xor
   //Serial1.println("<E>");//caractère de fin de chaine
